b11a63f002587280e3de008094418d9940e27e3f
BenMortensenTTS <ttttloading@gmail.com>
2019-09-02 16:29:34 -0700

    added local storage for wins

--- src/App.js
+++ src/App.js
@@ -18,7 +18,9 @@ class App extends React.Component {
       totalP1Flipped: 0,
       totalP2Flipped: 0,
       p1Total: 0,
-      p2Total: 0
+      p2Total: 0,
+      p1Wins: 0,
+      p2Wins: 0
     }
   }
 
@@ -36,6 +38,10 @@ class App extends React.Component {
 
 
   componentDidMount() {
+    let p1Wins = localStorage.getItem('p1Wins')
+    let p2Wins = localStorage.getItem('p2Wins')
+
+
     let deckIndexArr = this.genDeck();
     let p1hand = [];
     let p2hand = [];
@@ -73,6 +79,8 @@ class App extends React.Component {
       play2Hand: p2hand,
       cardArray: deckImageArr,
       discard: discardObj,
+      p1Wins,
+      p2Wins
     })
   }
 
@@ -90,36 +98,62 @@ class App extends React.Component {
   }
 
   calculateScore = (item) => {
-    let state = this.state;
-    let total1 = 0;
-    let total2 = 0;
+    if(this.state.playing) {
+      let state = this.state;
+      let total1 = 0;
+      let total2 = 0;
 
-    for(let i = 0; i < 3; i++) {
-      if(state.play1Hand[i].id % 13 === state.play1Hand[i+3].id % 13) {
-        total1 += 0;
-      } else {
-        total1 += (state.play1Hand[i].id % 13) + (state.play1Hand[i+3].id % 13)
+      for(let i = 0; i < 3; i++) {
+        if(state.play1Hand[i].id % 13 === state.play1Hand[i+3].id % 13) {
+          total1 += 0;
+        } else {
+          total1 += (state.play1Hand[i].id % 13) + (state.play1Hand[i+3].id % 13)
+        }
+        if(state.play2Hand[i].id % 13 === state.play2Hand[i+3].id % 13) {
+          total2 += 0;
+        } else {
+          total2 += (state.play2Hand[i].id % 13) + (state.play2Hand[i+3].id % 13)
+        }
       }
-      if(state.play2Hand[i].id % 13 === state.play2Hand[i+3].id % 13) {
-        total2 += 0;
-      } else {
-        total2 += (state.play2Hand[i].id % 13) + (state.play2Hand[i+3].id % 13)
+
+      this.setState({
+        p1Total: total1,
+        p2Total: total2,
+        playing: false
+      })
+
+     if (total2 > total1) {
+      let curr = parseInt(this.state.p2Wins) + 1
+      this.setState({p2Wins: curr}, () => {
+        if(parseInt(this.state.p2Wins) > 8) {
+          localStorage.setItem('p2Wins', 0);
+          localStorage.setItem('p1Wins', 0);
+          this.setState({p2Wins: 0})
+          this.setState({p1Wins: 0})
+        } else {
+          localStorage.setItem('p2Wins', curr);
+        }
+      })
+    }
+
+      else if (total1 > total2) {
+        let curr = parseInt(this.state.p1Wins) + 1
+        this.setState({p1Wins: curr}, () => {
+          localStorage.setItem('p1Wins', curr);
+          if(parseInt(this.state.p1Wins) > 8) {
+            localStorage.setItem('p1Wins', 0);
+            localStorage.setItem('p2Wins', 0);
+            this.setState({p1Wins: 0})
+            this.setState({p2Wins: 0})
+          }
+        })
       }
     }
-    console.log("Player 1: " + total1)
-    console.log("Player 2: " + total2)
-
-    this.setState({
-      p1Total: total1,
-      p2Total: total2,
-      playing: false
-    })
-
   }
 
 
   render() {
- 
+
   const getCardFromDeck = (item) => {
     if(this.state.playing && this.state.cardArray.length > 0) {
       console.log(item.target)
@@ -164,17 +198,27 @@ class App extends React.Component {
   }
 
     let renderPlay1Hand = this.state.play1Hand.map((card1, index)=>{
-      return <img onClick={player1Click} src={"./images/0.jpg"} id={card1.id} className={index} key={index+10} alt="card" />
+      return <img draggable onClick={player1Click} src={"./images/0.jpg"} id={card1.id} className={index} key={index+10} alt="card" />
     })
+    let play1HandTop = renderPlay1Hand.slice(0,3);
+    let play1HandBot = renderPlay1Hand.slice(3);
+
     let renderPlay2Hand = this.state.play2Hand.map((card2, index)=>{
-      return <img onClick={player2Click} src={"./images/0.jpg"} id={card2.id} className={index} key={index+100} alt="card"/>
+      return <img draggable="true" onClick={player2Click} src={"./images/0.jpg"} id={card2.id} className={index} key={index+100} alt="card"/>
     })
+
+    let play2HandTop = renderPlay2Hand.slice(0,3);
+    let play2HandBot = renderPlay2Hand.slice(3);
+
     let deck = <img onClick={getCardFromDeck} src={"./images/0.jpg"} id={this.state.deckCardID} alt="deck" />
     let discard = <img src={this.state.discard.image} id={this.state.discard.id} alt="discard"/>
     let cardsOrEnd = this.state.cardArray.length !== 0 ? <h3>{"Amount of cards left in deck: " + this.state.cardArray.length}</h3> : <h3>No more cards.</h3>
     let player1Score = <h3>{"Player 1 Score: " + this.state.p1Total}</h3>
     let player2Score = <h3>{"Player 2 Score " + this.state.p2Total}</h3>
     let winner;
+    let p1Wins = <h3>{"Player 1 Wins: " + this.state.p1Wins}</h3>
+    let p2Wins = <h3>{"Player 2 Wins: " + this.state.p2Wins}</h3>
+
 
     if(this.state.p1Total > this.state.p2Total) {
       winner = <h3>{"The Winner is Player 1!"}</h3>
@@ -188,17 +232,34 @@ class App extends React.Component {
 
     return (
       <div>
-        <div>{renderPlay1Hand}</div>
-        <div>{renderPlay2Hand}</div>
+        <div id="cards">
+          <div>{play1HandTop}</div>
+          <div>{play1HandBot}</div>
+        
+          <span className="spanMove">Discard</span><span className="spanMove">Deck</span><div></div>
+          <span className="spanMove">{discard}</span><span className="spanMove">{deck}</span>
 
-        <span>Discard</span><span>Deck</span><div></div>
-        <span>{discard}</span><span>{deck}</span>
-        <div>{cardsOrEnd}</div>
-        <div>{player1Score}</div>
-        <div>{player2Score}</div>
-        <Button clickFunc={this.startGame} buttonName="Start Game"/>
-        <Button clickFunc={this.calculateScore} buttonName="Calculate Score"/>
-        <div>{winner}</div>
+          <div>{play2HandTop}</div>
+          <div>{play2HandBot}</div>
+
+        </div>
+
+        <div id="info">
+          
+          <Button clickFunc={this.startGame} buttonName="Start Game"/>
+          <Button clickFunc={this.calculateScore} buttonName="Calculate Score"/> 
+
+          <div>{cardsOrEnd}</div>
+          <div>{player1Score}</div>
+          <div>{player2Score}</div>
+
+          <div>{winner}</div> 
+          <div id="wins"><span>{p1Wins}{p2Wins}</span></div>
+
+        </div>
+
+        
+
       </div>
     );
   }
